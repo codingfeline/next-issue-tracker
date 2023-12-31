8a6ed8f4e75e376c146838343192e86218242112
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-12-31 23:04:27 +0000

    add issue page template

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,34 @@
-// 'use cient'
+'use client'
 
-import React from 'react'
+import SimpleMDE from 'react-simplemde-editor'
+import 'easymde/dist/easymde.min.css'
 import { TextArea, TextFieldRoot, TextFieldInput, Button } from '@radix-ui/themes'
+import { Controller, useForm } from 'react-hook-form'
+import { string } from 'zod'
+
+interface IssueForm {
+  title: string
+  description: string
+}
 
 const NewIssuePage = () => {
+  const { register, control } = useForm<IssueForm>()
+
   return (
     <>
       <h2>New Issue Page</h2>
-      <div className="max-w-xl space-y-2">
+      <form className="max-w-xl space-y-2">
         <TextFieldRoot>
-          <TextFieldInput placeholder="Title" />
+          <TextFieldInput placeholder="Title" {...register('title')} />
         </TextFieldRoot>
-        <TextArea placeholder="Reply to comment…" />
+        {/* <TextArea placeholder="Reply to comment…" /> */}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+        />
         <Button>Submit New Issue</Button>
-      </div>
+      </form>
     </>
   )
 }
