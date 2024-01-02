676f003afb9d9b63d3b7de5793ec6548bbd29385
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-01-02 05:41:31 +0000

    add edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { prisma } from '@/prisma/client'
 import { notFound } from 'next/navigation'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import ReactMarkDown from 'react-markdown'
+import { Pencil2Icon } from '@radix-ui/react-icons'
+import Link from 'next/link'
 
 interface Props {
   params: { id: string }
@@ -19,18 +21,26 @@ const IssueDetail = async ({ params }: Props) => {
   if (!issue) notFound()
 
   return (
-    <>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="2" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        {/*  the above is to overcome tailwind h1 issue, after adding 
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="2" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          {/*  the above is to overcome tailwind h1 issue, after adding 
          require('@tailwindcss/typography') */}
-        <ReactMarkDown>{issue.description}</ReactMarkDown>
-      </Card>
-    </>
+          <ReactMarkDown>{issue.description}</ReactMarkDown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   )
 }
 
