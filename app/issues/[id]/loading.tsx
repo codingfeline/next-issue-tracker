bc989e5c448686354af605c726bd5e2f77dcd08f
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-01-02 00:03:20 +0000

    add more skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import IssueStatusBadge from '@/app/components/IssueStatusBadge'
+import { Heading, Flex, Card, Box } from '@radix-ui/themes'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 const LoadingIssueDetail = () => {
   return (
-    <>
-      <p>Loading Issue Detail</p>
-    </>
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="2" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        {/*  the above prose is to overcome tailwind h1 issue, after adding 
+       require('@tailwindcss/typography') */}
+        <Skeleton count={3} />
+      </Card>
+    </Box>
   )
 }
 
