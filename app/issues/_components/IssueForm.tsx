e4f9463b6976d03777d5ba1d1f83d34a330a0a8b
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-01-03 22:44:29 +0000

    improve the loading skeletons

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -14,15 +14,16 @@ import {
 } from '@radix-ui/themes'
 import axios from 'axios'
 import 'easymde/dist/easymde.min.css'
-import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
+import SimpleMDE from 'react-simplemde-editor'
 import { z } from 'zod'
 
-const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
-  ssr: false,
-}) // ? this is to disable SSR for loading MD editor as it is a client component
+// const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+//   ssr: false,
+// })
+// ? this is to disable SSR for loading MD editor as it is a client component
 
 type IssueFormData = z.infer<typeof issueSchema>
 
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,14 +1,2 @@
-import { Box } from '@radix-ui/themes'
-import React from 'react'
-import { Skeleton } from '@/app/components'
-
-const LoadingNewIssue = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton />
-      <Skeleton height="20rem" />
-    </Box>
-  )
-}
-
-export default LoadingNewIssue
+import IssueFormSkeleton from '../_components/IssueFormSkeleton'
+export default IssueFormSkeleton
