6e94bc52c177a0c0fe28087f38fab4b2ae8cff75
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-01-06 23:40:40 +0000

    refactor the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,12 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
 import { useSession } from 'next-auth/react'
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes'
+import { usePathname } from 'next/navigation'
 
 export default function NavBar() {
-  const currentPath = usePathname()
-  const { status, data: session } = useSession() // get user's name email image
-
-  const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues/list' },
-  ]
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -22,53 +15,78 @@ export default function NavBar() {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map(link => (
-                <li key={link.href}>
-                  <Link
-                    className={` ${
-                      link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'
-                    } hover:text-zinc-800 transition-colors`}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-
-          <Box>
-            {status === 'authenticated' ? (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                    // * above is to combar Avatar not displaying.
-                    // * it works during dev without the above but just in case
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2"></Text>
-                    {session.user!.email}
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Logout</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            ) : (
-              <Link href="/api/auth/signin">SignIn</Link>
-            )}
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
+  )
+}
+
+const NavLinks = () => {
+  const currentPath = usePathname()
+
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues/list' },
+  ]
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map(link => (
+        <li key={link.href}>
+          <Link
+            className={` ${link.href === currentPath && '!text-zinc-900'} nav-link`}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession() // get user's name email image
+
+  if (status === 'loading') return null
+
+  if (status === 'unauthenticated')
+    return (
+      <Link className="nav-link" href="/api/auth/signin">
+        SignIn
+      </Link>
+    )
+
+  return (
+    <Box>
+      {status === 'authenticated' && (
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user!.image!}
+              fallback="?"
+              size="2"
+              radius="full"
+              className="cursor-pointer"
+              referrerPolicy="no-referrer"
+              // * above is to combar Avatar not displaying.
+              // * it works during dev without the above but just in case
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2"></Text>
+              {session.user!.email}
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">Logout</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      )}
+    </Box>
   )
 }
