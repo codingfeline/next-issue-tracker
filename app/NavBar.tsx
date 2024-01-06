4e71da2298f4934fe6e55dd5b543053fcbf38d0b
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-01-06 11:39:21 +0000

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,9 +3,12 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
+import { useSession } from 'next-auth/react'
+import { Box } from '@radix-ui/themes'
 
 export default function NavBar() {
   const currentPath = usePathname()
+  const { status, data: session } = useSession() // get user's name email image
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -18,17 +21,25 @@ export default function NavBar() {
       </Link>
       <ul className="flex space-x-6">
         {links.map(link => (
-          <Link
-            key={link.href}
-            className={` ${
-              link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'
-            } hover:text-zinc-800 transition-colors`}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              className={` ${
+                link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'
+              } hover:text-zinc-800 transition-colors`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' ? (
+          <Link href="/api/auth/signout">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">SignIn</Link>
+        )}
+      </Box>
     </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Container, Theme, ThemePanel } from '@radix-ui/themes'
 import NavBar from './NavBar'
+import AuthProvider from './api/auth/Provider'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -20,13 +21,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="purple">
-          <NavBar />
-          <main className="p-5">
-            <Container>{children}</Container>
-          </main>
-          {/* <ThemePanel /> */}
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor="purple">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+            {/* <ThemePanel /> */}
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )
